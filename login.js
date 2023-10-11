f634b8523fff8811b8969a0fba0407b03f5d4b8e
YukiRisa2608 <nham26081992@gmail.com>
2023-10-11 20:31:57 +0900

    commit

---
+++ login.js
@@ -0,0 +1,19 @@
+function login(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const list_accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+    //kiểm tra xem email và password có đúng hay k
+    let index = list_accounts.findIndex((v) => v.email === email && v.password === password)
+    if (email.length <= 0 || password.length <= 0){
+      confirm('Please check your email or password and try again.')
+      return;
+    }
+    if (index > -1){
+        //đăng nhập thành công
+        localStorage.setItem("account_login", JSON.stringify(list_accounts[index]))
+        window.location.href = 'http://127.0.0.1:5500/project_modul01_book_store/homePage.html';
+    } else {
+        confirm("Please check your email or password and try again.");
+    }
+}
